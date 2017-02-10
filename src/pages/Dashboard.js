9e37f868137f1b09e479ae343ecb45b2b8b5dd8a
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2017-02-10 18:40:00 +0100

    add icons

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,11 @@
 import './dashboard.scss';
+import Icons from '../components/Icons';
 
 export default {
+  components: {
+    ...Icons
+  },
+
   template: `
     <div class="dashboard">
       <header class="header">
@@ -10,19 +15,19 @@ export default {
           :router="true"
           :default-active="$route.path">
           <el-menu-item index="/articles">
-            <img src="/all-articles-icon.svg" />
+            <all-articles-icon />
             {{$t('menu.allArticlesItem')}}
           </el-menu-item>
           <el-menu-item index="/articles/drafts">
-            <img src="/drafts-icon.svg" />
+            <drafts-icon />
             {{$t('menu.draftsItem')}}
           </el-menu-item>
           <el-menu-item index="/articles/published">
-            <img src="/published-icon.svg" />
+            <published-icon />
             {{$t('menu.publishedItem')}}
           </el-menu-item>
           <el-menu-item index="/settings">
-            <img src="/settings-icon.svg" />
+            <settings-icon />
             {{$t('menu.settingsItem')}}
           </el-menu-item>
         </el-menu>
