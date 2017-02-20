3fa9da9d690af227e26d115d4eaa52867682007b
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2017-02-20 10:54:35 +0100

    disable analytics reconnection on dev

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -3,7 +3,10 @@ import config from 'c0nfig';
 
 // connect to analytics server
 robAnalyticsSDK
-  .config({url: config.analytics.url})
+  .config({
+    url: config.analytics.url,
+    reconnection: (config.env !== 'development')
+  })
   .install();
 
 export default robAnalyticsSDK;
